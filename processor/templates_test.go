382ef560aa886f6ee5692736546338e9c1e6ef74
rupor-github <rupor0mail@gmail.com>
2025-08-01 09:34:12 -0400

    document.v2: handle empty series for opf-title

--- processor/templates_test.go
+++ processor/templates_test.go
@@ -18,6 +18,7 @@ type testCaseTmpl struct {
 	context string
 	arg     any
 	tmpl    []byte
+	fb2     []byte
 	result  string
 }
 
@@ -62,16 +63,18 @@ const titleTemplate = `template = """\
 {{-   join ", " $all -}}\
 {{- /* Abbreviated series/number + book title */ -}}\
 {{- else -}}\
-{{-   $seriesLetters := list -}}\
-{{-   range $word := splitList " " .Series.Name -}}\
-{{-     $seriesLetters = append $seriesLetters (upper (first (splitList "" $word))) -}}\
-{{-   end -}}\
-{{-   if gt (len $seriesLetters) 0 -}}\
-{{-     "(" }}{{- join "" $seriesLetters -}}\
-{{-     if gt .Series.Number 0 -}}\
-{{-       printf " - %02d" .Series.Number -}}\
+{{-   if .Series.Name -}}\
+{{-     $seriesLetters := list -}}\
+{{-     range $word := splitList " " .Series.Name -}}\
+{{-       $seriesLetters = append $seriesLetters (upper (first (splitList "" $word))) -}}\
 {{-     end -}}\
-{{-     ") " -}}\
+{{-     if gt (len $seriesLetters) 0 -}}\
+{{-       "(" }}{{- join "" $seriesLetters -}}\
+{{-       if gt .Series.Number 0 -}}\
+{{-         printf " - %02d" .Series.Number -}}\
+{{-       end -}}\
+{{-       ") " -}}\
+{{-     end -}}\
 {{-   end -}}\
 {{    .Title -}}\
 {{- end -}}\
@@ -115,43 +118,57 @@ var tests = []testCaseTmpl{
 				{{   if .MiddleName }} {{ .MiddleName }}{{ end }}\
 			  {{ end -}}\
 		      """`),
+		fb2:    []byte(fb2),
 		result: "Иванов Иван Иванович",
 	},
 	{
 		context: "opf-title",
 		tmpl:    []byte(titleTemplate),
+		fb2:     []byte(fb2),
 		result:  "(Т - 01) Тестовая книга",
+	},
+	{
+		context: "opf-title",
+		tmpl:    []byte(titleTemplate),
+		fb2:     []byte(fb2_no_series),
+		result:  "Тестовая книга",
 	},
 	{
 		context: "section-title",
 		tmpl:    []byte(titleTemplate),
+		fb2:     []byte(fb2),
 		result:  "Иванов Иван Иванович и др Тестовая книга",
 	},
 	{
 		context: "stamp-title",
 		tmpl:    []byte(titleTemplate),
+		fb2:     []byte(fb2),
 		result:  "Иванов Иван Иванович и др",
 	},
 	{
 		context: "toc-title",
 		tmpl:    []byte(titleTemplate),
+		fb2:     []byte(fb2),
 		result:  "Иванов Иван Иванович, Петров Пётр Петрович, Сидоров Сидор Сидорович",
 	},
 	{
 		context: "output-filename",
 		tmpl:    []byte(filenameTemplate),
+		fb2:     []byte(fb2),
 		result:  "Иванов Иван Иванович и др - Тестовая книга",
 	},
 	{
 		context: "link-notes",
 		arg:     NoteDefinition{Name: "n_17", Number: 0, NoteNumber: 17},
 		tmpl:    []byte(linkNoteTemplate),
+		fb2:     []byte(fb2),
 		result:  "[17]",
 	},
 	{
 		context: "link-notes",
 		arg:     NoteDefinition{Name: "n_17", Number: 22, NoteNumber: 17},
 		tmpl:    []byte(linkNoteTemplate),
+		fb2:     []byte(fb2),
 		result:  "[22.17]",
 	},
 	{
@@ -185,6 +202,7 @@ var tests = []testCaseTmpl{
 					{{- $parts = append $parts $last -}}\
 					{{- join "/" $parts -}}\
 	            """`),
+		fb2:    []byte(fb2),
 		result: "Иванов Иван Иванович/Тесты/Иванов 01 Тестовая книга",
 	},
 }
@@ -220,17 +238,18 @@ func TestTemplates(t *testing.T) {
 		Cfg: cfg,
 	}
 
-	p, err := NewFB2(bytes.NewReader(fb2), false, "path1/path2/file.fb2", "/path3/path4", false, false, false, OEpub, env)
-	if err != nil {
-		t.Fatalf("Unable to parse book: %v", err)
-	}
-
-	err = p.Process()
-	if err != nil {
-		t.Fatalf("Unable to process book: %v", err)
-	}
-
 	for i, c := range tests {
+
+		p, err := NewFB2(bytes.NewReader(c.fb2), false, "path1/path2/file.fb2", "/path3/path4", false, false, false, OEpub, env)
+		if err != nil {
+			t.Fatalf("Unable to parse book: %v", err)
+		}
+
+		err = p.Process()
+		if err != nil {
+			t.Fatalf("Unable to process book: %v", err)
+		}
+
 		enc := toml.NewEncoder()
 		var v map[string]string
 		if err := enc.Decode(c.tmpl, &v); err != nil {
@@ -255,7 +274,7 @@ var testConfig = []byte(`
 	version = 2
 `)
 
-var fb2 = []byte(`<?xml version="1.0" encoding="utf-8"?>
+const fb2 = `<?xml version="1.0" encoding="utf-8"?>
 <FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:l="http://www.w3.org/1999/xlink">
 
  <description>
@@ -335,4 +354,85 @@ var fb2 = []byte(`<?xml version="1.0" encoding="utf-8"?>
   </section>
  </body>
 </FictionBook>
-`)
+`
+
+const fb2_no_series = `<?xml version="1.0" encoding="utf-8"?>
+<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:l="http://www.w3.org/1999/xlink">
+
+ <description>
+  <title-info>
+   <genre>det_classic</genre>
+   <author>
+    <first-name>Иван</first-name>
+    <middle-name>Иванович</middle-name>
+    <last-name>Иванов</last-name>
+   </author>
+   <author>
+    <first-name>Пётр</first-name>
+    <middle-name>Петрович</middle-name>
+    <last-name>Петров</last-name>
+   </author>
+   <author>
+    <first-name>Сидор</first-name>
+    <middle-name>Сидорович</middle-name>
+    <last-name>Сидоров</last-name>
+   </author>
+   <book-title>Тестовая книга</book-title>
+   <annotation>
+    <p>Здесь полагается быть <strong>краткому</strong>, но весьма <strong>живописному</strong> описанию<sup><a l:href="#n_17" type="note">[17]</a></sup> предлагаемой читателю книги.</p>
+    <p>Как правило, с действительностью ничего общего не имеет. Чистая реклама в классическом стиле – «Вам сегодня необыкновенно повезло!»</p>
+    <empty-line/>
+    <p>Для разнообразия в этой аннотации будет указано истинное назначение книги – тестирование возможности корректной конвертации формата FB2 в любой другой формат, понимаемый прежде всего электронными книгами (eBooks).</p>
+    <p>В данной тестовой книге используется различное стилевое оформление текста, сноски, стихотворения, таблицы, изображения (в том числе с прозрачностью) и т.п.</p>
+    <p>Книга <strong>не</strong> является полностью валидной с точки зрения наиболее распространённых FB2-редакторов. К сожалению, количество косяков в существующих FB2-книгах превосходит все разумные пределы, и это приходится учитывать.</p>
+    <p>Свои пожелания по добавлению категорически необходимых плюшек можно отправлять на форум «The-eBook» в личку <strong><a l:href="http://www.the-ebook.org/forum/profile.php?mode=viewprofile&amp;u=9389">старику Кацу</a></strong>.</p>
+   </annotation>
+   <date>26.10.2015</date>
+   <coverpage>
+    <image l:href="#cover.jpg"/>
+   </coverpage>
+   <lang>ru</lang>
+   <src-lang>ru</src-lang>
+  </title-info>
+  <document-info>
+   <author>
+    <nickname>kaznelson</nickname>
+   </author>
+   <program-used>Book Designer 5.0, FictionBook Editor Release 2.6, AkelPad 4</program-used>
+   <date value="201-105-26">26.10.2015</date>
+   <id>BD-A2B81E-20EB-3A4E-EE8F-1664-26E3-BEF744</id>
+   <version>2.4</version>
+  </document-info>
+ </description>
+
+ <body>
+  <title>
+   <p>Авторский коллектив</p>
+   <empty-line/>
+   <p>Тестовый FB2</p>
+  </title>
+
+  <section id="s_1">
+   <title>
+    <p>Эпиграф.</p>
+    <p>Текст.<sup><a l:href="#n_18" type="note">[18]</a></sup></p>
+    <p>Ударения.</p>
+   </title>
+
+   <epigraph>
+    <p>Спортсмены! Принимая низкий старт, убедитесь, что сзади никто не бежит с шестом!</p>
+    <text-author>Автор<sup><a l:href="#n_1" type="note">[1]</a></sup></text-author>
+   </epigraph>
+
+   <empty-line/>
+   <subtitle><emphasis>Перекрёстные ссылки</emphasis></subtitle>
+   <p><emphasis><a l:href="#s_2">Глава 2</a></emphasis></p>
+   <empty-line/>
+   <p id="p_1">Выше дана перекрёстная ссылка на секцию с заголовком (<emphasis>&lt;section id="s_2"&gt;</emphasis> + <emphasis>&lt;title&gt;</emphasis>).</p>
+   <p>Пример ссылки между параграфами (<emphasis>&lt;p id="p_2"&gt;</emphasis>), ведущий в последний параграф этой главы: <emphasis>[<a l:href="#p_2">перекрёстная ссылка</a>]</emphasis>.</p>
+   <p>Пример ссылки между параграфами, ведущий в сноску главы «Надстрочник и подстрочник»: <emphasis>[<a l:href="#p_3">перекрёстная ссылка</a>]</emphasis>.</p>
+   <empty-line/>
+  </section>
+ </body>
+</FictionBook>
+`
